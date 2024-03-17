c588b101339d00eaf4437dce5b39cf6c69d4018b
Christian Duarte <w.christian.duarte@outlook.com>
2024-03-17 14:58:49 -0700

    Added animatable element for scroll animations

--- src/function/section.ts
+++ src/function/section.ts
@@ -4,14 +4,16 @@ export function checkIntersection (element: HTMLElement) {
     let options = {
         root: null,
         rootMargin: "0px",
-        threshold: .5,
+        threshold: .7
     };
 
     let observer = new IntersectionObserver((entries) => {
-        if(entries[0].intersectionRatio > options.threshold){
+        if(entries[0].intersectionRatio >= .7){
+            console.log(entries)
             const { sectionName } = element.dataset;
             currentSection = sectionName;
             document.querySelector('[basically-suffix]').textContent = currentSection;
+            console.log('a')
         }
     }, options);
     observer.observe(element)
