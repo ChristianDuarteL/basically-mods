2402b78321f7e5877184e17066ec4b3b818268c8
Christian Duarte <w.christian.duarte@outlook.com>
2024-04-14 14:41:53 -0700

    Changed link to swiper bundle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import { build } from 'esbuild';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   devToolbar: {
     enabled: false
-  }
+  },
 });
