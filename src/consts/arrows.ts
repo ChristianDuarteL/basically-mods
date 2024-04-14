f6d29ff67ab083390a92d674427ea52cd07d555f
Christian Duarte <w.christian.duarte@outlook.com>
2024-04-14 15:03:29 -0700

    Fixed typo

--- src/consts/arrows.ts
+++ src/consts/arrows.ts
@@ -12,7 +12,7 @@ export const ARROWS: Arrow[] = [
         description: 'A very magical arrow, beware it can penetrate(almost) anything.'
     },
     {
-        name: 'Torche arrow',
+        name: 'Torch arrow',
         slug: 'torch_arrow',
         description: ['Combining a torch and an arrow, now you can make arrows that place torches on the block they land.', 'What would it do to players?']
     }
