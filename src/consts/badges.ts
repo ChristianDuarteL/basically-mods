c441a5ff02b1841bf1d1281ee186853d89987533
Christian Duarte <w.christian.duarte@outlook.com>
2024-04-15 16:51:12 -0700

    fi dark mode badges

--- src/consts/badges.ts
+++ src/consts/badges.ts
@@ -2,19 +2,19 @@ import type { BadgeTypeMap } from "../types/BadgeTypes";
 
 export const BADGE_TYPES: BadgeTypeMap = {
     MOD: {
-        classes: 'bg-purple-200 text-black/70 font-semibold py-1 px-2 rounded-full inline text-sm',
+        classes: 'bg-purple-200 dark:bg-purple-900 text-black/70 dark:text-white/80 font-semibold py-1 px-2 rounded-full inline text-sm',
         text: 'MOD'
     },
     CLIENT: {
-        classes: 'bg-red-200 text-black/70 font-semibold py-1 px-2 rounded-full inline text-sm',
+        classes: 'bg-red-200 dark:bg-red-900 text-black/70 dark:text-white/80 font-semibold py-1 px-2 rounded-full inline text-sm',
         text: 'CLIENT'
     },
     SERVER: {
-        classes: 'bg-yellow-200 text-black/70 font-semibold py-1 px-2 rounded-full inline text-sm',
+        classes: 'bg-yellow-200 dark:bg-yellow-900 text-black/70 dark:text-white/80 font-semibold py-1 px-2 rounded-full inline text-sm',
         text: 'SERVER'
     },
     CLIENT_AND_SERVER: {
-        classes: 'bg-blue-200 text-black/70 font-semibold py-1 px-2 rounded-full inline text-sm',
+        classes: 'bg-blue-200 dark:bg-blue-900 text-black/70 dark:text-white/80 font-semibold py-1 px-2 rounded-full inline text-sm',
         text: 'CLIENT/SERVER'
     }
 }
